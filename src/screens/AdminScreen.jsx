d0954dc35856a7f1d1ba981fb33230bcc863fcab
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-23 03:19:21 +0530

    admin

--- src/screens/AdminScreen.jsx
+++ src/screens/AdminScreen.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 function AdminScreen() {
+  const userState = useSelector((state) => state.loginUserReducer);
+  const { currentUser } = userState;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!currentUser.isAdmin) {
+      window.location.href = "/";
+    }
+  });
   return (
     <div>
-        <h3 style={{fontSize:"35px"}}>Admin Panel</h3>
+      <div className="row justify-content-center">
+        <div className="col-md-10">
+          <h3 style={{ fontSize: "35px" }}>Admin Panel</h3>
+          <ul className="admin-function">
+            <li>
+              <a href="">Users List</a>
+            </li>
+            <li>
+              <a href="">Burgers List</a>
+            </li>
+            <li>
+              <a href="">Add New Burger</a>
+            </li>
+            <li>
+              <a href="">Orders List</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminScreen
+export default AdminScreen;
